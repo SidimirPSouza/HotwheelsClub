b1e77bd15e554c319c63cded1cf8c4fde3668a21
Sidi098 <sidimirh@hotmail.com>
2025-05-09 12:05:37 -0300

    First Commit

---
+++ HotwheelsClub/Data/HotwheelsClubDBContext.cs
@@ -0,0 +1,21 @@
+﻿using HotwheelsClub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotwheelsClub.Data
+{
+    public class HotwheelsClubDBContext : DbContext
+    {
+        public HotwheelsClubDBContext(DbContextOptions<HotwheelsClubDBContext> options) 
+            :base(options)
+        { 
+        }
+
+        public DbSet<HotwheelsModel> Hotwheels { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ HotwheelsClub/Models/HotwheelsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HotwheelsClub.Models
+{
+    public class HotwheelsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Modelo { get; set; }
+        public double preco {  get; set; }
+        public string Cor {  get; set; }
+        public int Ano { get; set; }
+        public virtual UsuarioModel Dono { get; set; }
+    }
+}
